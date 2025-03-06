aa7d8e70cd640cb966bac303edaa6404713c53a6
vskesha <vskesha@gmail.com>
2025-03-06 18:46:08 +0200

    created view for user signup

--- forum/users/views.py
+++ forum/users/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .serializers import UserSerializer
+
 
 # Create your views here.
+class SignupView(APIView):
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            if user:
+                # send email here
+                return Response(
+                    {
+                        "message": "User created successfully",
+                        "user_id": user.id,
+                    },
+                    status=status.HTTP_201_CREATED,
+                )
+            else:
+                return Response(
+                    {"message": "Failed to create user"},
+                    status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                )
+
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
