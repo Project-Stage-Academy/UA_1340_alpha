23f5d082c8f5e4ad7bb2487fef59f0bd84998d6c
vskesha <vskesha@gmail.com>
2025-03-08 22:16:02 +0200

    created send-email/ endpoint for testing purposes

--- forum/forum/urls.py
+++ forum/forum/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from users.views import SendEmailAPIView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('send-email/', SendEmailAPIView.as_view(), name="send_email"),
 ]
--- forum/users/views.py
+++ forum/users/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from forum.tasks import send_email_task
+
+
+class SendEmailAPIView(APIView):
+    def post(self, request):
+        subject = request.data.get("subject")
+        message = request.data.get("message")  # Plain text version
+        html_message = request.data.get("html_message")  # HTML version
+        recipient = request.data.get("recipient")  # List of emails
+
+        if not subject or not message or not recipient:
+            return Response({"error": "Missing fields"}, status=status.HTTP_400_BAD_REQUEST)
+
+        send_email_task.delay(subject, message, [recipient], html_message)
+
+        return Response({"success": "HTML Email is being sent"}, status=status.HTTP_202_ACCEPTED)
