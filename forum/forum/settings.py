185873c8980d1b9c151d60b1a4012bfecfcf7381
vskesha <vskesha@gmail.com>
2025-03-08 22:12:26 +0200

    settings updated with email and celery

--- forum/forum/settings.py
+++ forum/forum/settings.py
@@ -39,6 +39,7 @@ EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")  # Set in Docker
 
 # Celery settings
 CELERY_BROKER_URL = os.environ.get("CELERY_BROKER_URL")
+CELERY_RESULT_BACKEND = os.environ.get("CELERY_RESULT_BACKEND")
 CELERY_ACCEPT_CONTENT = ["json"]
 CELERY_TASK_SERIALIZER = "json"
 
