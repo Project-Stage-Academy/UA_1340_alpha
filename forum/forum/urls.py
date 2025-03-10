d4f48788245f9cecdbdd4c9f67d13262fd9ab797
Vasyl Boliukh <89039235+Vskesha@users.noreply.github.com>
2025-03-10 18:56:43 +0200

    Update urls.py

--- forum/forum/urls.py
+++ forum/forum/urls.py
@@ -26,6 +26,6 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-    path('api/users/', include('users.urls'))
+    path('api/users/', include('users.urls')),
     path('send-email/', SendEmailAPIView.as_view(), name="send_email"),
 ]
