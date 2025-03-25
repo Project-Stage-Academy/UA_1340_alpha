cac693f73dd761948d4e125692e859ae667b2c5e
Taras Sushko <burunduk9090@gmail.com>
2025-03-25 12:52:43 +0200

    UPD forum\forum\asgi.py

--- forum/forum/asgi.py
+++ forum/forum/asgi.py
@@ -4,7 +4,7 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from communications.routing import websocket_urlpatterns
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'forum.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
