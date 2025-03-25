291948c8f49c929fcc7d5db4403bbe08665b580a
Taras Sushko <burunduk9090@gmail.com>
2025-03-25 12:54:27 +0200

    resolve isorts problem

--- forum/forum/asgi.py
+++ forum/forum/asgi.py
@@ -1,8 +1,9 @@
 import os
-from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
+
 from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 from communications.routing import websocket_urlpatterns
+from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'forum.settings')
 
