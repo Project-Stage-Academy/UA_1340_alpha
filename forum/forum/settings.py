c218a95e6a27d94001909635507b22faef01dadd
Vlad Bukhta <bukhta05@gamail.com>
2025-03-29 20:15:13 +0200

    Add jwt middleware

--- forum/forum/settings.py
+++ forum/forum/settings.py
@@ -140,7 +140,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "allauth.account.middleware.AccountMiddleware",
+    'allauth.account.middleware.AccountMiddleware',
+    'users.middleware.JWTAuthMiddleware',
 ]
 
 ROOT_URLCONF = 'forum.urls'
